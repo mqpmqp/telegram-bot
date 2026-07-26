628338a8517c970a73197407b3f7cf0db1dd2ed1
Paulo Wakasa <31721989+mqpmqp@users.noreply.github.com>
2026-07-27 01:56:02 +0800

    test: complete Telegram adapter test double

--- tests/gateway/test_telegram_thread_fallback.py
+++ tests/gateway/test_telegram_thread_fallback.py
@@ -101,6 +101,7 @@ _fake_telegram_ext.CallbackQueryHandler = object
 _fake_telegram_ext.MessageHandler = object
 _fake_telegram_ext.TypeHandler = object
 _fake_telegram_ext.ContextTypes = SimpleNamespace(DEFAULT_TYPE=object)
+_fake_telegram_ext.ApplicationHandlerStop = RuntimeError
 _fake_telegram_ext.filters = object
 _fake_telegram_request = types.ModuleType("telegram.request")
 _fake_telegram_request.HTTPXRequest = object
